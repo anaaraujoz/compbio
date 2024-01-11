345ff2e1acde847dd2a266969377046764e7b4c3
Ana Araujo <anaaraujoz@outlook.com>
2024-01-11 05:07:49 -0300

    Adds goodwin project beginning

--- goodwin/goodwin.py
+++ goodwin/goodwin.py
@@ -4,16 +4,20 @@ import matplotlib.pyplot as plt
 from scipy.integrate import odeint
 import matplotlib.animation as animation
 
+# TODO make sliders for all the parameters
+# TODO steady state solver
+# TODO remember to update the steady state
 aix = 72
-Ai = 36
-ki = 1
+Ai = 10
+ki = 1 # most import slider
 bi = 2
 aiy = 1
-Bi = 0
+Bi = 1
 params = [aix, Ai, ki, bi, aiy, Bi]
 
-y0 = [7,-10]
-t = np.linspace(0, 300, num=100)
+y0 = [2,2]
+t = np.linspace(0, 100, num=200)
+
 
 def simNonInteractive(variables, t, params, ):
 
@@ -41,17 +45,18 @@ def simInteractive(variables, t):
     Y1 = variables[2]
     Y2 = variables[3]
 
+    # both k12 and k22 must be >0 for the model to be interactive
     a1x = 360
     A1 = 36
     b1 = 10
     k11 = 1
-    k12 = 0
+    k12 = 0.1
     a1y = 0.5
     B1 = 0
     a2x = 360
     A2 = 43
     b2 = 10
-    k21 = 0
+    k21 = 0.1
     k22 = 1
     a2y = 0
     B2 = 0
@@ -64,24 +69,23 @@ def simInteractive(variables, t):
 
     return([rX1, rX2, rY1, rY2])
 
+# TODO figure out how to bound the function to be always f>0.
 yNonInteractive = odeint(simNonInteractive, y0, t, args=(params,))
 
-yInteractive = odeint(simInteractive, y00, t, args=(), full_output=1)
+#yInteractive = odeint(simInteractive, y00, t, args=(), full_output=1)
 
-
+# Make plot X(t) x Y(t)
 fig, (ax1, ax2) = plt.subplots(2)
 
 # Non Interactive 
+# TODO plot steady state line reference
 line1 = ax1.plot(t, yNonInteractive[:,0], color="r", label="rate of mRNA transcription")
 line2 = ax1.plot(t, yNonInteractive[:,1], color="b", label="rate of protein production")
 
+# TODO make line3 color update over time, gradient
+line3 = ax2.plot(yNonInteractive[:,0], yNonInteractive[:,1], color="b", label="rateX by rateY")
+
 ax1.legend()
-
-line3 = ax2.plot(t, yInteractive[:,0], color="r", label="X1")
-line4 = ax2.plot(t, yInteractive[:,1], color="b", label="X2")
-line5 = ax2.plot(t, yInteractive[:,2], color="g", label="Y1")
-line6 = ax2.plot(t, yInteractive[:,3], color="b", label="Y2")
-
 ax2.legend()
 
 plt.show()
